0f7adbe03305b8b607848941f74bfea5db60ec40
Avinash Shrivastav <146878016+Avinash77068@users.noreply.github.com>
2025-02-25 15:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import express from "express";
 const app = express();
 import cors from "cors";
@@ -38,45 +38,4 @@ dbConnection()
   });
 
 
-=======
-import express from "express";
-const app = express();
-import cors from "cors";
-import { default as mongoose } from "mongoose";
-import { configDotenv } from "dotenv";
-import dbConnection from "./src/database/index.js";
-import employee from "./src/routes/employee.js"
-import user from "./src/routes/user.js"
-import authRouter from "./src/routes/auth.js"
-configDotenv();
-// Middleware Connections
-app.use(cors());
-app.use(express.json());
-const api='/api/v1/'
-// Mongo DB Connections
-app.use(express.urlencoded({extended:true}))
-// Routes
-app.get("/", async (req, res) => {
-  console.log("hi from the server");
-  return res.send({ message: "hi from the server" });
-});
-
-app.use(`${api}employee`,employee)
-app.use(`${api}user`,user)
-app.use(`${api}auth`, authRouter)
-// Connection
-const PORT = process.env.PORT || 5000;
-
-dbConnection()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`server is running on: http://127.0.0.1:${PORT}`);
-    });
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-
->>>>>>> 9aa4fe9af5039d6bc34d7790545c9f20d25d8d94
   export default app
